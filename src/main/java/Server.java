c8fbee0804a4863a06800a15509efbf806b0ddf4
andrey <tweetandnews@gmail.com>
2024-11-10 14:04:25 +0300

    asd

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -4,42 +4,24 @@ import java.net.Socket;
 import java.util.Arrays;
 
 public class Server {
-
-    public Server() {
-
-    }
-
-    public void Run() {
+    public void Start() {
         try (var serverSocket = new ServerSocket(4444)) {
-            //в цикле обрабатываем входящие соединения.
-//            while (true) {
+            while (serverSocket.isBound()) {
+                Socket socket = serverSocket.accept();
 
-            //метод accept ждет, пока кто-то не подключится.
-            Socket socket = serverSocket.accept();
-
-//            var br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            var dis = new DataInputStream(socket.getInputStream());
-//            System.out.println(socket.getInetAddress().getHostAddress());
-            System.out.println(dis.readUTF());
-
-//            DataInputStream dis = new DataInputStream(socket.getInputStream());
-
-//            String fileName = dis.readUTF();
-//            long fileSize = dis.readLong();
-
-
-//            DataInputStream din = new DataInputStream(socket.getInputStream());
-//            FileOutputStream fos = new FileOutputStream("loaded/" + fileName, false);
-//
-//            byte[] buffer = new byte[2048];
-//            int bytesRead;
-//            while ((bytesRead = din.read(buffer)) != -1) {
-//                System.out.println(bytesRead);
-//                fos.write(buffer, 0, bytesRead);  // Записываем только реальные байты
-//            }
-//
-//            fos.close();
-//            }
+                new Thread(() -> {
+                    try {
+                        var br = new BufferedInputStream(socket.getInputStream());
+                        int bytesRead = 0;
+                        byte[] buffer = new byte[1024];
+                        while ((bytesRead = br.read(buffer, 0, buffer.length)) != -1) {
+                            System.out.print("Получено от клиента:" + new String(buffer, 0, bytesRead));
+                        }
+                    } catch (IOException e) {
+                        System.out.println(e.getMessage());
+                    }
+                }).start();
+            }
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
