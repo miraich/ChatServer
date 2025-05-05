43935e955aeb574f8ab409b2a58fa087a5bc1ea3
andrey <tweetandnews@gmail.com>
2025-05-05 12:59:27 +0300

    refactor

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -7,17 +7,11 @@ import java.net.Socket;
 import static Static.Connections.clientHandlers;
 
 public class Server {
-    private ServerSocket messageServerSocket;
-//    private ServerSocket userServerSocket;
-
-    //    private static final int USER_PORT = 4445;
     private static final int MESSAGE_PORT = 4450;
 
     public void start() {
         try {
-            messageServerSocket = new ServerSocket(MESSAGE_PORT);
-//            userServerSocket = new ServerSocket(USER_PORT);
-
+            ServerSocket messageServerSocket = new ServerSocket(MESSAGE_PORT);
             while (!messageServerSocket.isClosed()) {
                 try {
                     Socket messageSocket = messageServerSocket.accept();
@@ -29,25 +23,6 @@ public class Server {
                     e.printStackTrace();
                 }
             }
-
-//            new Thread(() -> {
-//                while (!userServerSocket.isClosed()) {
-//                    try {
-//                        Socket userSocket = userServerSocket.accept();
-//                        System.out.println("Новое подключение для пользователя: " + userSocket);
-//                        for (ClientHandler handler : clientHandlers) {
-//                            if (handler.getMessageSocket().getInetAddress()
-//                                    .equals(userSocket.getInetAddress())) {
-//                                handler.setUserSocket(userSocket);
-//                                break;
-//                            }
-//                        }
-//                    } catch (IOException e) {
-//                        e.printStackTrace();
-//                    }
-//                }
-//            }).start();
-
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/main/java/handler/ClientHandler.java
+++ src/main/java/handler/ClientHandler.java
@@ -1,17 +1,13 @@
 package handler;
 
 import java.io.*;
-import java.net.DatagramPacket;
 import java.net.Socket;
-import java.util.HashSet;
-import java.util.Set;
 
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dto.MessageDTO;
-import model.User;
 
 import static Static.Connections.*;
 
@@ -19,23 +15,12 @@ public class ClientHandler implements Runnable {
     private final ObjectMapper objectMapper;
     private BufferedOutputStream messageBos;
     private BufferedInputStream messageBis;
-    //    private BufferedOutputStream userBos;
-//    private BufferedInputStream userBis;
-    private User serverUser;
     private final Socket messageSocket;
-    private Socket userSocket;
     private boolean connected;
-    private MessageDTO messageDTO;
-    private final Set<User> users = new HashSet<>();
-    private final DatagramPacket audioPacket;
-    private final byte[] audioBuffer = new byte[4096];
     private final byte[] messageInfoBuffer = new byte[1024];
-    private final byte[] userInfoBuffer = new byte[1024];
 
     public ClientHandler(Socket messageSocket) {
         this.messageSocket = messageSocket;
-//        this.userSocket = userSocket;
-        this.audioPacket = new DatagramPacket(audioBuffer, audioBuffer.length);
         JsonFactory jsonFactory = new JsonFactory();
         jsonFactory.configure(JsonGenerator.Feature.AUTO_CLOSE_TARGET, false);
         jsonFactory.configure(JsonParser.Feature.AUTO_CLOSE_SOURCE, false);
@@ -45,10 +30,6 @@ public class ClientHandler implements Runnable {
                 messageBos = new BufferedOutputStream(messageSocket.getOutputStream());
                 messageBis = new BufferedInputStream(messageSocket.getInputStream());
             }
-//            if (userSocket != null) {
-//                userBos = new BufferedOutputStream(userSocket.getOutputStream());
-//                userBis = new BufferedInputStream(userSocket.getInputStream());
-//            }
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -57,8 +38,7 @@ public class ClientHandler implements Runnable {
     @Override
     public void run() {
         connected = true;
-//        new Thread(this::handleUser).start();
-        new Thread(this::handleMessage).start();
+        handleMessage();
     }
 
     private void handleMessage() {
@@ -69,7 +49,7 @@ public class ClientHandler implements Runnable {
                 if (bytesRead == -1) {
                     break;
                 }
-                messageDTO = objectMapper.readValue(messageInfoBuffer, MessageDTO.class);
+                MessageDTO messageDTO = objectMapper.readValue(messageInfoBuffer, MessageDTO.class);
                 broadcastMessage(messageDTO);
             } catch (IOException e) {
                 e.printStackTrace();
@@ -77,73 +57,6 @@ public class ClientHandler implements Runnable {
             }
         }
     }
-
-//    private void handleUser() {
-//        int bytesRead;
-//        while (true) {
-//            try {
-//                bytesRead = userBis.read(userInfoBuffer, 0, userInfoBuffer.length);
-//                if (bytesRead == -1) {
-//                    break;
-//                }
-//                UserDTO userDTO = objectMapper.readValue(userInfoBuffer, UserDTO.class);
-//                serverUser = new User(userDTO.getUsername());
-//                serverUser.setId(User.counter.getAndIncrement());
-//                addServerUsersToSet(users);
-//                broadcastUsers();
-////                broadcastAudio();
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//        }
-//    }
-
-    //        int bytesRead;
-//        connected = true;
-//        while (true) {
-//            try {
-//                bytesRead = bis.read(messageBuffer, 0, messageBuffer.length);
-//                if (bytesRead == -1) {
-//                    break;
-//                }
-//                JsonNode jsonNode = objectMapper.readTree(messageBuffer);
-//                if (jsonNode.has("username")) { // Проверяем, есть ли поле username
-//                    UserDTO userDTO = objectMapper.treeToValue(jsonNode, UserDTO.class);
-//                    serverUser = new User(userDTO.getUsername());
-//                    serverUser.setId(User.counter.getAndIncrement());
-//                    addServerUsersToSet(users);
-//                    broadcastUsers();
-//                    broadcastAudio();
-//
-//                    users.clear();
-//                } else if (jsonNode.has("message")) {
-//                    messageDTO = objectMapper.treeToValue(jsonNode, MessageDTO.class);
-//                    broadcastMessage(messageDTO);
-//                } else {
-//                    System.out.println("Unknown data type received");
-//                }
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//                break;
-//            }
-//        }
-//        if (!clientHandlers.isEmpty()) {
-//            closeAll(bis, bos, socket);
-//            clientHandlers.remove(this);
-//            addServerUsersToSet(users);
-//            broadcastUsers();
-//        }
-
-//    private void broadcastUsers() {
-//        clientHandlers.forEach((client) -> {
-//            try {
-//                objectMapper.writeValue(client.userBos, users);
-//                client.userBos.flush();
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        });
-//    }
 
     private void broadcastMessage(MessageDTO messageDTO) {
         new Thread(() -> clientHandlers.forEach((client) -> {
@@ -154,32 +67,6 @@ public class ClientHandler implements Runnable {
                 e.printStackTrace();
             }
         })).start();
-    }
-
-//    private void broadcastAudio() {
-//        new Thread(() -> {
-//            try (DatagramSocket audioSocket = new DatagramSocket(50005)) {
-//                while (connected) {
-//                    audioSocket.receive(audioPacket);
-//                    for (ClientHandler client : clientHandlers) {
-//                        if (client == this && socket.isConnected()) continue;
-//                        DatagramPacket sendPacket = new DatagramPacket(audioPacket.getData(),
-//                                audioPacket.getLength(),
-//                                audioPacket.getAddress(),
-//                                audioPacket.getPort());
-//                        audioSocket.send(sendPacket);
-//                        System.out.println("Send audio to " + audioPacket.getAddress() + ":" + audioPacket.getPort() + " " +
-//                                client.serverUser.getUsername());
-//                    }
-//                }
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//        }).start();
-//    }
-
-    private void addServerUsersToSet(Set<User> userSet) {
-        clientHandlers.forEach((client) -> userSet.add(client.serverUser));
     }
 
     private void closeAll(BufferedInputStream bis, BufferedOutputStream bos, Socket socket) {
@@ -197,13 +84,5 @@ public class ClientHandler implements Runnable {
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    public void setUserSocket(Socket userSocket) {
-        this.userSocket = userSocket;
-    }
-
-    public Socket getMessageSocket() {
-        return messageSocket;
     }
 }
